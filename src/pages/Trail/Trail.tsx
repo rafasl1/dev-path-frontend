d04a886102316e455fd5d8d8e2b0843560342065
Rafael Silva de Lima <rafaellima1100@gmail.com>
2022-10-16 18:05:49 -0300

    Adiciona lista de tópicos de estudo

--- src/pages/Trail/Trail.tsx
+++ src/pages/Trail/Trail.tsx
@@ -1,21 +1,31 @@
 import { TrailInfo } from "../../types/Trail";
+import './styles.css'
+
 
 export function Trail({ title, hours, levels, topics, carrerInfo, salaryRange, opportunities }: TrailInfo) {
+
     return (
         <main>
 
             <div id="main-trail">
 
-                <h1>Trilha{" " + title}</h1>
+                <h1 id="main-trail-title">Trilha{" " + title}</h1>
 
-                <div>
-                    <p>Tempo estimado: {hours}</p>
-                    <p>Níveis: {levels}</p>
+                <div id="main-trail-subtitle">
+                    <b id="main-trail-subtitle-time">Tempo estimado: {hours}</b>
+                    <b>Níveis: {levels}</b>
                 </div>
 
                 <div id="main-trail-topics">
-                    <ul>
-                        {topics.map(topic => (<li>{topic}</li>))}
+                    <ul id="main-trail-topics-list">
+                        {topics.map((topic, index) => { 
+                            return (
+                                <div id="main-trail-topic-list-item">
+                                    <div id="main-trail-topic-ball"><p>{index + 1}</p></div>
+                                    <li id="main-trail-topic-text">{topic}</li>
+                                </div>
+                            )}
+                        )}
                     </ul>
                 </div>
 
@@ -29,14 +39,15 @@ export function Trail({ title, hours, levels, topics, carrerInfo, salaryRange, o
                 <p>Salários entre R${salaryRange[0]} - R${salaryRange[1]}</p>
 
                 <h1>Vagas na área:</h1>
-                {opportunities.map(opportunities => (
+                {opportunities.map(opportunities => {
+                    return (
                     <div>
                         <h3>{opportunities.title}</h3>
                         <p>{opportunities.location}</p>
                         <p>{opportunities.type}</p>
                         <p>{opportunities.seniority}</p>
                     </div>
-                ))}
+                ) })}
 
             </div>
 
