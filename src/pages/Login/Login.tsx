1c29e39a61d1481cfb546856df0d0daa0968aedc
Rafael <rafael.sl@usp.br>
2022-11-19 17:54:59 -0300

    Termina base da páginas de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,30 @@
+import { useForm } from "react-hook-form";
 import './styles.css'
 
 export function Login() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data: any) => console.log(data);
+
     return (
-        <h1>Login</h1>
+        <div id="registration-div-page">
+            <div id="registration-box">
+                <h1 id="registration-box-title">{"Faça seu Login!"}</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="input-form">
+                        <label>
+                            Email:<br/>
+                            <input className="input-text" {...register("email",  { required: true })} />   
+                        </label>
+                    </div>
+                    <div className="input-form">
+                        <label>
+                            Senha:<br/>
+                            <input className="input-text" type="password" {...register("password",  { required: true })} />    
+                        </label>
+                    </div>
+                    <input id="submit-button" type="submit"  />
+                </form>
+            </div>
+        </div>
     )
 }
