b045dce47c832dca2cd97c19ae01a5e9e3550f58
Rafael <rafael.sl@usp.br>
2022-11-19 17:50:38 -0300

    Termina a base da página de cadastro

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,7 +1,37 @@
+import { useForm } from "react-hook-form";
 import './styles.css'
 
 export function Registration() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data: any) => console.log(data);
+    console.log(watch("example"));
+
     return (
-        <h1>Cadastro</h1>
+        <div id="registration-div-page">
+            <div id="registration-box">
+                <h1 id="registration-box-title">{"Cadastre-se no <DevPath /> !"}</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="input-form">
+                        <label>
+                            Nome:<br/>
+                            <input className="input-text" {...register("name",  { required: true })}  />    
+                        </label>
+                    </div>
+                    <div className="input-form">
+                        <label>
+                            Email:<br/>
+                            <input className="input-text" {...register("email",  { required: true })} />   
+                        </label>
+                    </div>
+                    <div className="input-form">
+                        <label>
+                            Senha:<br/>
+                            <input className="input-text" type="password" {...register("password",  { required: true })} />    
+                        </label>
+                    </div>
+                    <input id="submit-button" type="submit"  />
+                </form>
+            </div>
+        </div>
     )
 }
---
+++ src/types/UserCreate.ts
@@ -0,0 +1,5 @@
+export type UserCreate = {
+    name: "string";
+    email: "string";
+    password: "string";
+}
