9af82883c3219aecedccc28786f8fc0b78520680
Rafael <rafael.sl@usp.br>
2022-11-20 19:46:06 -0300

    Dinamiza nome de usuário no perfil

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,12 +3,19 @@ import { useEffect, useState } from 'react'
 import Woman2 from '../../assets/woman(2)1.svg'
 import { TrailCard } from '../../components/TrailCard/TrailCard'
 import { TrailAPI } from '../../types/TrailAPI'
+import { UserAPI } from '../../types/User';
 import './styles.css'
 
 export function Profile() {
     const [trailsData, setTrailsData] = useState<Array<TrailAPI>>([])
+    const [userData, setUserData] = useState<UserAPI>()
 
     useEffect(() => {
+        const user = localStorage.getItem("loged-user")
+        if (user) {
+            setUserData(JSON.parse(user))
+        }
+
         getTrailsData()
     }, [])
     
@@ -23,7 +30,7 @@ export function Profile() {
             <div id='profile-user-info-div'>
                 <img id='user-avatar' src={Woman2} /><br/>
                 <button id='user-tag'>#Aluno</button>
-                <h1 id='profile-user-name'>Rayssa Eloá Aparecida</h1>
+                <h1 id='profile-user-name'>{userData?.name}</h1>
                 <button id='profile-user-became-mentor-button' onClick={() => alert("Nosso time está avaliando seu perfil!\nEntraremos em contato se acharmos que você pode ser um mentor na DevPath!")}>
                     Quero ser um mentor
                 </button>
