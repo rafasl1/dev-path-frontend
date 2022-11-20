c39d9deaf27c2a410fa09bf3b4b64e2d22f30afa
Rafael <rafael.sl@usp.br>
2022-11-20 19:47:38 -0300

    Adiciona botões de login e cadastro

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,9 @@ export function Header() {
             <div className='headerItems'>
                 <h1><Link to={"/"}>{"< DevPath />"}</Link></h1>
                 <PagTab content={"Find your path"} route={"find-your-path"}/>
-                <PagTab content={"Mentoring"}  route={"mentoria"}/>
+                <PagTab content={"Mentoria"}  route={"mentoria"}/>
+                <PagTab content={"Login"}  route={"login"}/>
+                <PagTab content={"Cadastre-se!"}  route={"cadastro"}/>
             </div>
         </header>
     )
