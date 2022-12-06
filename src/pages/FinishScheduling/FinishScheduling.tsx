86f99d1993fe9cae127db47f2368318f48449c1d
Rafael Silva de Lima <rafaellima1100@gmail.com>
2022-12-05 22:15:28 -0300

    Melhora os textos da página de finalizar agendamento

--- src/pages/FinishScheduling/FinishScheduling.tsx
+++ src/pages/FinishScheduling/FinishScheduling.tsx
@@ -18,8 +18,9 @@ export function FinishScheduling() {
             <h2 className="finish-subtitle blue">Próximas instruções: </h2>
             <ul>
                 <li className="next-instruction-item"><h3>O valor total da mentoria para o horário solicitado foi de <b className="important-value">R$50,00</b></h3></li>
-                <li className="next-instruction-item"><h3>O pagamento deverá ser realizado por transferência PIX para o CPF do mentor: <b className="important-value">451.621.160-25</b></h3></li>  
-                <li className="next-instruction-item"><h3>Quando o mentor receber a sua transferência, ele entrará confirmará a sua solicitação de agendamento</h3></li>  
+                <li className="next-instruction-item"><h3>O pagamento deverá ser realizado por transferência PIX para o CNPJ da DevPath: <b className="important-value">53.370.689/0001-02</b></h3></li>  
+                <li className="next-instruction-item"><h3>Assim que a DevPath receber a sua transferência, o mentor receberá o pagamento referente ao seu agendamento e seu agendamento será concluído</h3></li>  
+                <li className="next-instruction-item"><h3>Quando o agendamento for concluído, o mentor será notificado, e basta aguardar para receber sua mentoria!</h3></li>  
             </ul>
 
             <br />
